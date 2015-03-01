5867be361d9129a5b34c8b7bb84ac09644671a7e
Jacob Penner <jkpenner91@gmail.com>
2015-03-01 11:39:34 -0800

    Initial RPGStatType Enum

---
+++ Assets/Scripts/RPGStatType.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Enum used as a list to contin the IDs for each individual RPGStat 
+/// </summary>
+public enum RPGStatType {
+    None                        = 0,
+
+    Vital_Health                = 1001,
+    Vital_Mana                  = 1002,
+
+    Skill_Melee_Defence         = 2002,
+    Skill_Melee_Range           = 2003,
+    Skill_Melee_Magic           = 2004,
+
+    Attribute_Constitution      = 3001,
+    Attribute_Speed             = 3002,
+    Attribute_Willpower         = 3003,
+}
