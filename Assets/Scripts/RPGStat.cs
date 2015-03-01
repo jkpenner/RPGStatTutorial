c1dead7f8d747b70db33a1920a0bad2eec2211cb
Jacob Penner <jkpenner91@gmail.com>
2015-03-01 11:31:35 -0800

    Initial RPGStat Class

---
+++ Assets/Scripts/RPGStat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPGStat {
+    private string _statName;
+    private int _statBaseValue;
+
+    public RPGStat() {
+        _statName = string.Empty;
+        _statBaseValue = 0;
+    }
+
+    public string StatName {
+        get { return _statName; }
+        set { _statName = value; }
+    }
+
+    public virtual int StatBaseValue {
+        get { return _statBaseValue; }
+        set { _statBaseValue = value; }
+    }
+
+    public virtual int StatValue {
+        get { return StatBaseValue; }
+    }
+}
