b2722187ef0b87d9c11d84cfcd03b2214e2754c4
Jacob Penner <jkpenner91@gmail.com>
2015-03-01 12:54:59 -0800

    Initial Basic Interfaces
    Add interfaces: IStatUpdatable, IStatInitializable, IStatProgressable

---
+++ Assets/Scripts/Interfaces/IStatInitalizable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IStatInitalizable {
+    void OnInitialize(RPGEntity entity);
+}
---
+++ Assets/Scripts/Interfaces/IStatProgressable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IStatProgressable {
+    void UpdateStat(int level);
+}
