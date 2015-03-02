89c0d2b1f26945bc054f91f367ab7d62a5e5bb7d
Jacob Penner <jkpenner91@gmail.com>
2015-03-01 21:59:32 -0800

    Initial RPGAttribute Class

---
+++ Assets/Scripts/StatTypes/RPGAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPGAttribute : RPGStatModifiable, IStatProgressable {
+    private int _statLevelValue;
+
+    public int StatLevelValue {
+        get { return _statLevelValue; }
+    }
+
+    public override int StatValue {
+        get { return base.StatValue + _statLevelValue; }
+    }
+
+    public virtual void UpdateStat(int level) {
+        _statLevelValue = level;
+    }
+}
